0c33e3ba00c6aebfafeaa5825ffdb61e0b3d1257
JaikoSuno <juan.quispe06@gmail.com>
2019-01-07 03:36:20 -0300

    modificando el archivo master.blade

--- resources/views/aprendizajes/create.blade.php
+++ resources/views/aprendizajes/create.blade.php
@@ -1,37 +1,35 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Registrar Actividad de Aprendizaje + Servicio</h1>
+    <h3>Registrar Actividad de Aprendizaje + Servicio</h3>
 
     <form method="POST" action="{{ url('aprendizaje') }}">
         {{ csrf_field() }}
-        * Campos obligatorios
         <br>
         <label for="asignatura">Nombre de la asignatura:</label>
-        <input type="text" name="asignatura" id="asignatura" placeholder="Asignatura">
-        *
+        <input type="text" name="asignatura" id="asignatura" placeholder="Asignatura" required>
+        <small id="advertencia_asignatura" class="form-text text-muted"> Hola </small>
         <br>
         <label for="nombre">Nombre del profesor:</label>
-        <input type="text" name="nombre" id="nombre" placeholder="Juan Perez">
-        *
+        <input type="text" name="nombre" id="nombre" placeholder="Juan Perez" required>
         <br>
         <label for="cantidad">Cantidad Estudiantes:</label>
-        <input type="text" name="cantidad" id="cantidad">
-        *
+        <input type="text" name="cantidad" id="cantidad" required>
+
         <br>
         <label for="socio">Nombre del Socio Comunitario:</label>
-        <input type="text" name="socio" id="socio" placeholder="Diego Perez">
-        *
+        <input type="text" name="socio" id="socio" placeholder="Diego Perez" required>
+
         <br>
-        <laber for="año">Año:</laber>
-        <input type="text" name="año", id="año">
-        *
+        <label for="año">Año:</label>
+        <input type="text" name="año", id="año" required>
+
         <br>
         <label for="semestre">Semestre:</label>
-        <input type="text" name="semestre" id="semestre" placeholder="1 ó 2">
-        *
+        <input type="text" name="semestre" id="semestre" placeholder="1 ó 2" required>
+
         <br>
-        <label for="evidencia">Listado de participantes:*</label>
+        <label for="evidencia">Listado de participantes:</label>
         <input type="file" name="evidencia" accept=".pdf, .png, .jpg, .jpeg">
         <br>
         <button type="submit">Registrar Actividad</button>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,99 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <div class="jumbotron text-center">
+        <h1> Universidad Catolica del Norte</h1>
+    </div>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>@yield('title') SB Admin 2 - Bootstrap Admin Theme</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href={{asset("vendor/bootstrap/css/bootstrap.min.css")}} rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href={{asset("vendor/metisMenu/metisMenu.min.css")}} rel="stylesheet">
-
-    <!-- Morris Charts CSS -->
-    <link href={{asset("vendor/morrisjs/morris.css")}} rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href={{asset("vendor/font-awesome/css/font-awesome.min.css")}} rel="stylesheet" type="text/css">
-
-    <!-- Bootstrap Core CSS
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <!-- MetisMenu CSS
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/metisMenu/3.0.3/metisMenu.css" rel="stylesheet">
-    -->
-    <!-- Custom CSS -->
-    <link href={{asset("css/sb-admin-2.min.css")}} rel="stylesheet">
-
-    <!-- Morris Charts CSS
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
-
-    <!-- Custom Fonts
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
- -->
-    <script src={{asset("vendor/jquery/jquery.min.js")}}></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src={{asset("vendor/bootstrap/js/bootstrap.min.js")}}></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src={{asset("vendor/metisMenu/metisMenu.min.js")}}></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src={{asset("vendor/raphael/raphael.min.js")}}></script>
-    <script src={{asset("vendor/morrisjs/morris.min.js")}}></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src={{asset("js/sb-admin-2.min.js")}}></script>
 
 </head>
 
 <body>
 
 
-    <div id="wrapper">
-
-        <!-- Navigation -->
-        @include('layouts.nav')
-
-        <div id="page-wrapper">
-
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Dashboard</h1>
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
+    <div class = "container">
             <div class="row">
                 @yield('content')
             </div>
 
-        </div>
-        <!-- /#page-wrapper -->
-
     </div>
+        <!-- /#page-wrapper -->
     <!-- /#wrapper -->
 
     <!-- jQuery -->
 
 
-
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 </body>
 
 </html>
